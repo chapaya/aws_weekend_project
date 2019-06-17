ad2b31cf8b1c7a8e5eb171016e65f81b1795f50d
eran <egrimberg@gmail.com>
2019-06-17 21:07:03 +0300

    describe instances

--- instance_status.py
+++ instance_status.py
@@ -1,11 +1,25 @@
-
+#https://stackoverflow.com/questions/46379043/boto3-wait-until-running-doesnt-work-as-desired
 import boto3
 
 ec2 = boto3.resource('ec2')
 instance = ec2.Instance('i-0a5448168783adce5')
 print(instance.state)
+print(instance.state['Name'])  # instance status
 print(instance.launch_time)
 print(instance.image_id)
+instance.wait_until_running()
+print(instance.state)
+
 
 for instance in ec2.instances.all():
     print(instance.id, instance.state)
+
+
+'''
+{'Code': 16, 'Name': 'running'}
+running
+2019-06-17 17:04:16+00:00
+ami-024a64a6685d05041
+{'Code': 16, 'Name': 'running'}
+i-0a5448168783adce5 {'Code': 16, 'Name': 'running'}
+'''
